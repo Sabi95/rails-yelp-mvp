5f7b48a153ab8eeda4ba3da067855ff6063cb79e
Savithri Wewala <s.w.wewala@gmail.com>
2022-11-09 16:56:41 +0900

    finished all functions and basic design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,37 @@
 require 'faker'
 
-puts 'Cleaning out DB...'
+puts 'Cleaning out Restaurant DB...'
 Restaurant.destroy_all
 
 puts 'Creating new Restaurants'
-5.times do
-  Restaurant.create!(
-    name: Faker::Ancient.primordial,
-    address: Faker::Address.street_address,
-    phone_number: Faker::PhoneNumber.phone_number,
-    category: Restaurant::CUISINE.sample
-  )
-end
+
+  5.times do
+    Restaurant.create!(
+      name: Faker::Games::Zelda.location,
+      address: Faker::Address.street_address,
+      phone_number: Faker::PhoneNumber.phone_number,
+      category: Restaurant::CUISINE.sample
+    )
+  end
 
 puts "...created #{Restaurant.count} restaurants"
+
+puts 'Cleaning out Reviews DB...'
+Review.destroy_all
+
+puts 'Creating new Reviews'
+
+Restaurant.all.each do |restaurant|
+  2.times do
+    Review.create!(
+    rating: rand(0..5),
+    content: Faker::Restaurant.review,
+    restaurant: restaurant
+    )
+  end
+end
+
+puts "...created #{Review.count} reviews"
 
 #   movies = Movie.create({ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
